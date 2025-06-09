9992633db4632468e52be297e6300304adc341ae
Mike Snowden <5297545+MikeTheSnowman@users.noreply.github.com>
2025-06-10 03:28:45 +1000

    another initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to Schrödinger Pharmaceuticals</h1>
+<p>We totally (don't) manufacture and sell pharmaceuticals!</p>
+<p>Do we actually have any medicine? Maybe we do, maybe we don't!</p>
+
