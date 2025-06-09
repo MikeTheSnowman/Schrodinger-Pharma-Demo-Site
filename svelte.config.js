331a9ef2d160abd311c4a14dbf9251e5809a13fa
Mike Snowden <5297545+MikeTheSnowman@users.noreply.github.com>
2025-06-10 03:33:23 +1000

    no adapter test

--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,21 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		// See below for an explanation of these options
-		config: undefined,
-		platformProxy: {
-			configPath: undefined,
-			environment: undefined,
-			persist: undefined
-		},
-		fallback: 'plaintext',
-		routes: {
-			include: ['/*'],
-			exclude: ['<all>']
-		}
-	}) 
+	kit: { 
+	// 	adapter: adapter({
+	// 	// See below for an explanation of these options
+	// 	config: undefined,
+	// 	platformProxy: {
+	// 		configPath: undefined,
+	// 		environment: undefined,
+	// 		persist: undefined
+	// 	},
+	// 	fallback: 'plaintext',
+	// 	routes: {
+	// 		include: ['/*'],
+	// 		exclude: ['<all>']
+	// 	}
+	// }) 
 }
 };
 
